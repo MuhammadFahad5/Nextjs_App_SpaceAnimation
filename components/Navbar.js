1063312483354236e9d675903d02f456bddeea00
Muhammad Fahad <fahad199275@gmail.com>
2023-12-21 07:08:10 +0500

    Space Animation

---
+++ components/Navbar.js
@@ -0,0 +1,30 @@
+
+
+const Navbar = () => {
+    return (
+        
+        <header>
+        <div className="container">
+            <nav className="row">
+                <div className="logo">
+                    <img src="Ababeel! (3)-1.png" alt=""/>
+                        
+                </div>
+
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Skills</a></li>
+                    <li><a href="#">Portfolio</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+
+    </header>
+
+    )
+
+}
+
+export default Navbar;
